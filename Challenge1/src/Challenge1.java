c3896fca7b56e6bb6ac1cf40f03688f56495ebef
sujjad <53053362+sujjadshaik@users.noreply.github.com>
2021-02-05 09:07:30 +0530

    update challenge1

--- Challenge1/src/Challenge1.java
+++ Challenge1/src/Challenge1.java
@@ -3,7 +3,8 @@ public class Challenge1 {
 public static void main(String[] args) {
 	System.out.println("Challenge 1");
 	//feature1 code 
-	System.out.println("Challenge 1");// this is will throw conflict
+	System.out.println("Challenge 1");//thorws a conflict
+	
 
 }
 }
